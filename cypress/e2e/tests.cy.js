f2015ecfba9f6f14134312ce11a68d2b1777519c
Nuno Agostinho <nuno@ebi.ac.uk>
2023-04-17 00:23:52 +0100

    Remove wait commands

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,29 +1,24 @@
 describe('ShinyProxy', () => {
   it('main', () => {
     cy.visit()
-    cy.wait(2000)
     cy.screenshot()
   })
 
   it('psichomics', () => {
     cy.visit()
-    cy.wait(2000)
     cy.contains('psichomics').click()
     cy.screenshot()
     
     cy.visit('/psichomics')
-    cy.wait(2000)
     cy.screenshot()
   })
   
   it('cTRAP', () => {
     cy.visit()
-    cy.wait(2000)
     cy.contains('cTRAP').click()
     cy.screenshot()
 
     cy.visit('/cTRAP')
-    cy.wait(2000)
     cy.screenshot()
   })
 })
@@ -31,10 +26,8 @@ describe('ShinyProxy', () => {
 describe('Flower dashboard', () => {
   it('open', () => {
     cy.visit('http://localhost:5555/flower/dashboard')
-    cy.wait(1000)
     cy.screenshot()
     cy.contains('celery@ctrap').click()
-    cy.wait(1000)
     cy.screenshot()
   })
 })
@@ -42,7 +35,6 @@ describe('Flower dashboard', () => {
 describe('Grafana', () => {
   it('open', () => {
     cy.visit('http://localhost:3000')
-    cy.wait(1000)
     cy.screenshot()
   })
 })
@@ -50,7 +42,6 @@ describe('Grafana', () => {
 describe('Prometheus', () => {
   it('open', () => {
     cy.visit('http://localhost:9090')
-    cy.wait(1000)
     cy.screenshot()
   })
 })
@@ -58,7 +49,6 @@ describe('Prometheus', () => {
 describe('Plausible', () => {
   it('open', () => {
     cy.visit('http://localhost:8000')
-    cy.wait(1000)
     cy.screenshot()
   })
 })
