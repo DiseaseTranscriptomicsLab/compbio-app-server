62aa00a78e583dcb9bd37fe31469b36c907e97a4
Nuno Agostinho <nuno@ebi.ac.uk>
2023-04-17 00:00:02 +0100

    Add Cypress tests

--- cypress/e2e/tests/sample_spec.cy.js
+++ cypress/e2e/tests/sample_spec.cy.js
@@ -1,10 +1,59 @@
-describe('My First Test', () => {
-  it('Test Flower dashboard', () => {
+describe('ShinyProxy', () => {
+  it('main', () => {
+    cy.visit('http://localhost')
+    cy.screenshot()
+  })
+
+  it('psichomics', () => {
+    cy.visit('http://localhost')
+    cy.contains('psichomics').click()
+    cy.screenshot()
+    
+    cy.visit('http://localhost/psichomics')
+    cy.screenshot()
+  })
+  
+  it('cTRAP', () => {
+    cy.visit('http://localhost')
+    cy.contains('cTRAP').click()
+    cy.screenshot()
+
+    cy.visit('http://localhost/cTRAP')
+    cy.screenshot()
+  })
+})
+
+describe('Flower dashboard', () => {
+  it('open', () => {
     cy.visit('http://localhost:5555/flower/dashboard')
-    cy.wait(2000) // wait 2 seconds
+    cy.wait(1000)
     cy.screenshot()
     cy.contains('celery@ctrap').click()
-    cy.wait(2000) // wait 2 seconds
+    cy.wait(1000)
+    cy.screenshot()
+  })
+})
+
+describe('Grafana', () => {
+  it('open', () => {
+    cy.visit('http://localhost:3000')
+    cy.wait(1000)
+    cy.screenshot()
+  })
+})
+
+describe('Prometheus', () => {
+  it('open', () => {
+    cy.visit('http://localhost:9090')
+    cy.wait(1000)
+    cy.screenshot()
+  })
+})
+
+describe('Plausible', () => {
+  it('open', () => {
+    cy.visit('http://localhost:8000')
+    cy.wait(1000)
     cy.screenshot()
   })
 })
